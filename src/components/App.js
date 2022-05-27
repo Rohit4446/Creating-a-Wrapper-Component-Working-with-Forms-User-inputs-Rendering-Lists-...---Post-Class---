fb210c661823d0498d7fe4fe841b0c26125a5b1e
Rohit4446 <90442893+Rohit4446@users.noreply.github.com>
2022-05-27 17:05:12 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ul>
+          <li key={"relativeListItem1"}>fufa</li>
+          <li key={"relativeListItem2"}>mama</li>
+          <li key={"relativeListItem3"}>mami</li>
+          <li key={"relativeListItem4"}>cacaha</li>
+          <li key={"relativeListItem5"}>cachi</li>
+          <li key={"relativeListItem6"}>fufi</li>
+          <li key={"relativeListItem7"}>bhen</li>
+          <li key={"relativeListItem8"}>bhai</li>
+        </ul>
+      </div>
+    );
+  }
 }
-
 
 export default App;
