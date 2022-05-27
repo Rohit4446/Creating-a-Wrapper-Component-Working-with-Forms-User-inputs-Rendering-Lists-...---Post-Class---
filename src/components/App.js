8f2f3a98f415a4be59225b35cfd30d9d37c76edc
Rohit4446 <90442893+Rohit4446@users.noreply.github.com>
2022-05-27 17:13:59 +0530

    okk

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ class App extends Component {
     return (
       <div id="main">
         <ol>
-          <li key={"relativeListItem1"}>fufa</li>
-          <li key={"relativeListItem2"}>mama</li>
-          <li key={"relativeListItem3"}>mami</li>
-          <li key={"relativeListItem4"}>cacaha</li>
-          <li key={"relativeListItem5"}>cachi</li>
-          <li key={"relativeListItem6"}>fufi</li>
-          <li key={"relativeListItem7"}>bhen</li>
-          <li key={"relativeListItem8"}>bhai</li>
+          <li key={"fufa1"}>fufa</li>
+          <li key={"mama2"}>mama</li>
+          <li key={"mami3"}>mami</li>
+          <li key={"cacha4"}>cacha</li>
+          <li key={"cachi5"}>cachi</li>
+          <li key={"fufi6"}>fufi</li>
+          <li key={"bhen7"}>bhen</li>
+          <li key={"bhai8"}>bhai</li>
         </ol>
       </div>
     );
